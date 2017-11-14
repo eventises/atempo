92ee67825f0d680ae1c296cb6f1eedde10637124
Mikel <mikel@eventises.com>
2017-11-14 20:26:59 +0100

    link views

--- atempo/urls.py
+++ atempo/urls.py
@@ -3,4 +3,6 @@ from atempoapp import views
 
 urlpatterns = [
     url(r'^$', views.AtempoViewAll.as_view(), name='msg-list'),
+    url(r'new$', views.AtempoNewMsg.as_view(), name='msg-new'),
+    url(r'save$', views.AtempoSave.as_view(), name='msg-save'),
 ]
--- atempoapp/views.py
+++ atempoapp/views.py
@@ -1,10 +1,22 @@
 from django.views.generic import View
 from django.template.response import TemplateResponse
 from atempoapp.models import Atempo
+from django.http import HttpResponseRedirect
 
 class AtempoViewAll(View):
 	def get(self, request, *args, **kwargs):
 		context = {
-            'msg_list': Atempo.objects.all()
+            'msg_list': Atempo.objects.all().order_by('-id')
         }
 		return TemplateResponse(request, 'msglist.html', context)
+class AtempoNewMsg(View):
+	def get(self, request, *args, **kwargs):
+		return TemplateResponse(request, 'newmsg.html', {})
+
+class AtempoSave(View):
+	def post(self, request, *args, **kwargs):
+		name = request.POST['name']
+		message = request.POST['message']
+		email = request.POST['email']
+		Atempo.objects.create(name=name,message=message,email=email)
+		return HttpResponseRedirect('/')
